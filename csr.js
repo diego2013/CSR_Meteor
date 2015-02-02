b7fff39cf21834070bd1cddedc5cb1d255c407b6
Diego <diego@PHS000515.local>
2015-02-02 16:13:05 -0500

    added Publish and subscribe

--- csr.js
+++ csr.js
@@ -1,7 +1,9 @@
   Scenarios = new Mongo.Collection("scenarios");
 
 
+//CLIENT SIDE
 if (Meteor.isClient) {
+  Meteor.subscribe("scenarios");
   // counter starts at 0
   //Session.setDefault("counter", 0);
 
@@ -9,29 +11,30 @@ if (Meteor.isClient) {
   Template.body.helpers({
     scenarios: function () {
       return Scenarios.find({}, {sort: {createdAt: -1}});
-    }
+    },
+
+    isOwner: function () {
+      return this.owner === Meteor.userId();
+  }
   });
 
   Template.NewScenarioForm.events({
   "submit .new-scenario": function (event) {
    // "click .save-scenario": function (event) {
     // This function is called when the new scenario form is submitted
-    console.log("saving...");
+    //console.log("saving...");
     var title = event.target.title.value;
-    //var title = "title".value;
-    console.log(title);
-    //console.log(event);
-   // var description = event.target.description.value;
+    var description = event.target.description.value;
 
-    Scenarios.insert({
-      title: title,
-     // description: description,
-      createdAt: new Date() // current time
-    });
+    //var title = "title".value;
+    //console.log(title);
+    //console.log(event);
+    Meteor.call("saveScenario", title, description);
+    
 
     // Clear form
     event.target.title.value = "";
-    //event.target.description.value = "";
+    event.target.description.value = "";
 
     // Prevent default form submit
     return false;
@@ -41,14 +44,60 @@ if (Meteor.isClient) {
 Template.scenario.events({
   "click .delete": function () {
     console.log("deleting... "+this._id+" "+this.title);
-    Scenarios.remove(this._id);
+    Meteor.call("deleteScenario", this._id);
   }
 });
 
+  //to configure the accounts UI to use usernames instead of email addresses
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
 
+
+Meteor.methods({
+  saveScenario: function(title, description){
+    // Make sure the user is logged in before modifiing a scenario
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Scenarios.insert({
+      title: title,                     //title of the scenario
+      description: description,         //description of the scenario
+      createdAt: new Date(),            // current time
+      owner: Meteor.userId(),           // _id of logged in user
+      username: Meteor.user().username  // username of logged in user
+    });
+
+  },
+  deleteScenario: function(scnID){
+    var scenario = Scenarios.findOne(scnID);    //fetch
+    if (scenario.owner !== Meteor.userId()) {
+    // If the current user is not thescenario owner
+      throw new Meteor.Error("not-authorized");
+    }
+    Scenarios.remove(scnID);
+  }
+
+
+});
+
+
+//SERVER SIDE
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
+  Meteor.publish("scenarios", function () {
+    return Scenarios.find({owner: this.userId });
+
+    /* $or: [
+      { status: "public" },
+      { owner: this.userId }
+    ]*/
+  });
+
+  
 }
