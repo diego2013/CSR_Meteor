975a2fd03bfe04ff0df0932585a41671812de1f8
diego2013 <diego@mdpnp.org>
2016-02-09 12:51:12 -0500

    Addressed issue #144, find by ID functionality

--- client/views/scenario/findByIDTemplate.events.js
+++ client/views/scenario/findByIDTemplate.events.js
@@ -7,10 +7,23 @@ Template.findByIDTemplate.events({
     //event.preventDefault();
     //Fetch and process input
     var scenarioID = template.find("#findByIDbox").value;
+    console.log(scenarioID)
     scenarioID = scenarioID.trim();
     findByID(scenarioID); 
     //clear
     template.find("#findByIDbox").value = "";
+  }
+
+//on pressing "enter" on the "scneario ID" find text box
+  ,"keypress #findByIDbox" : function(event, template){
+      //Fetch and process input
+      if(event.keyCode==13){
+        var scenarioID = template.find("#findByIDbox").value;
+        scenarioID = scenarioID.trim();
+        findByID(scenarioID); 
+
+        event.preventDefault();
+      }
   }
 
 });
@@ -24,5 +37,17 @@ Template.findByIDErrorTemplate.events({
     findByID(scenarioID);
     //clear
     template.find("#findByIDbox").value = "";
+  }
+  
+  //on pressing "enter" on the "scneario ID" find text box
+  ,"keypress #findByIDbox" : function(event, template){
+      //Fetch and process input
+      if(event.keyCode==13){
+        var scenarioID = template.find("#findByIDbox").value;
+        scenarioID = scenarioID.trim();
+        findByID(scenarioID); 
+
+        event.preventDefault();
+      }
   }
 });
