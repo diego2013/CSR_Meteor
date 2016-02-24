06fb7172e0640ed3e554cde5fccb0c230d6cee09
diego2013 <diego@mdpnp.org>
2016-02-24 13:23:30 -0500

    Addressed issue #150. Send verification email upon account creation

--- server/smtp.js
+++ server/smtp.js
@@ -10,7 +10,37 @@
 
 
     process.env.MAIL_URL="smtp://md.pnp.team%40gmail.com:mdpnpCSR@smtp.gmail.com:465/"; 
-    //XXX check this SO Answer in order to know how to allow the less secure applications and allowe access to a GMAIL account.
+    //XXX check this SO Answer in order to know how to allow the less secure applications and allow access to a GMAIL account.
     // http://stackoverflow.com/a/31875371/3961519
     //XXX Also, probably the pwd should not be here in plain text
+    //use an environment variable to set process.env.MAIL_URL to keep passwords out of source control.
+
+
+  //Send verification email on user account creation
+  // Format the email
+  //-- Application name
+  Accounts.emailTemplates.siteName = 'Clinical Scenario Repository';
+
+  //-- Subject line of the email.
+  Accounts.emailTemplates.verifyEmail.subject = function(user) {
+    return "Confirm Your Email Address for the MD PnP's Clinical Scenario Repository";
+  };
+
+  //-- Email text
+  Accounts.emailTemplates.verifyEmail.text = function(user, url) {
+    var text = "Dear " +user.username +",\r\n\n"
+        + 'Thank you for registering with the Clinical Scenario Repository.\r\n'
+        + 'Please click on the following link to verify your email address: \r\n' 
+        + url
+        + "\r\n \n This email contains the username you used to register, so you can be sure it came from a trusted source."
+    return text;
+  };
+
+  // 3.  Send email when account is created
+  Accounts.config({
+    sendVerificationEmail: true
+    // ,forbidClientAccountCreation : false
+  });
+//XXX see also http://stackoverflow.com/q/14616524/3961519
+
   });
